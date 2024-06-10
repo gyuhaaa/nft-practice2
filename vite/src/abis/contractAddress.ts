97d4ca1aa9ed817c0556951fabf19b5ccd51e2a4
gyuhaaa <vkvkvk2892@gmail.com>
2024-06-10 17:29:13 +0900

    conn Metamask

--- vite/src/abis/contractAddress.ts
+++ vite/src/abis/contractAddress.ts
@@ -1,2 +1,2 @@
-export const mintContractAddress = "0x688c0611a5691B7c1F09a694bf4ADfb456a58Cf7";
-export const saleContractAddress = "0x929336a17aF293b16d025170e310d7C408C5447e";
+export const mintContractAddress = "0x611080E58464edfd0050Cd1E477BA2D273DCc532";
+export const saleContractAddress = "0xe9Bd224deebe83D72E30857e9044691Fe42666F4";
